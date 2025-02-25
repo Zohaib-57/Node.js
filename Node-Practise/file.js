69326d3471a76f11ce6f3373122804b0b04a85ae
Your Name <zohaibabbas8557@gmail.com>
2025-02-25 13:54:00 +0500

    node practise

---
+++ Node-Practise/file.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+
+//Synchronous way of writing a file
+fs.writeFileSync("./file.txt" ,"This is a file created by Node.js");
---
+++ Node-Practise/hello.js
@@ -0,0 +1,9 @@
+// console.log("hello world");
+
+const { add, sub } = require("./math.js");
+//  const math  = require("./math.js");
+// console.log("Maths Value is" , math);
+
+
+console.log(add(2, 6));
+console.log(sub(5, 3));
