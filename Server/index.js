1396b0d2a341dd2c5d5ffaac08160adcb05dbc2a
Your Name <zohaibabbas8557@gmail.com>
2025-02-26 14:21:16 +0500

    URL handling

--- Server/index.js
+++ Server/index.js
@@ -1,29 +1,40 @@
-
-//Creating a Server
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
+
 const myServer = http.createServer((req, res) => {
-	const log = `${Date.now(0)} : ${req.url} New Request Received\n`;
-    //taking care of the non-blocking request
-	fs.appendFile("log.txt", log, (err, data) => {
-        switch(req.url){
-            case "/":
-                res.end("Home Page");
-                break;
-            case "/about":
-                res.end("About Page");
-                break;
-            case "/contact":
-                res.end("Contact Page");
-                break;
-            default:
-                res.end("404 Page Not Found");
-                break;
-        }
+	const log = `${Date.now()} : ${req.url} New Request Received\n`;
+	const myUrl = url.parse(req.url, true);
+	console.log(myUrl);
+
+	// Handling routes properly
+	switch (myUrl.pathname) {
+		case "/":
+			res.end("Home Page");
+			break;
+		case "/about":
+			const username = myUrl.query.myname || "Guest";
+			res.end(`Hi, I am ${username}`);
+			break;
+		case "/contact":
+			res.end("Contact Page");
+			break;
+		case "/search":
+			const search = myUrl.query.search_query || "No search query provided";
+			res.end("Here are your results for: " + search);
+			break;
+		default:
+			res.statusCode = 404;
+			res.end("404 Page Not Found");
+			break;
+	}
+
+	// Append logs after response to avoid delaying requests
+	fs.appendFile("log.txt", log, (err) => {
+		if (err) console.error("Error writing to log file", err);
 	});
-	// console.log(req);
 });
-//whenever you make the changes in your server you have to restart your server
+
 myServer.listen(8000, () => {
 	console.log("Server is running on port 8000");
 });
